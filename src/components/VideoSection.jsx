238d30ad1726b0432e3ff84d044320076513236e
shashankack <shashank.ac.dev@gmail.com>
2025-06-17 12:57:58 +0530

    refactor: enhance VideoSection by adding refs for video elements; implement autoplay functionality with error handling

--- src/components/VideoSection.jsx
+++ src/components/VideoSection.jsx
@@ -1,4 +1,5 @@
-import { Box, useTheme, useMediaQuery, Grid } from "@mui/material";
+import { useRef, useEffect } from "react";
+import { useTheme, useMediaQuery, Grid } from "@mui/material";
 
 import horizontal1 from "../assets/videos/horizontal_1.mp4";
 import horizontal2 from "../assets/videos/horizontal_2.mp4";
@@ -10,11 +11,29 @@ const VideoSection = () => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
+  // Refs for all videos
+  const h1Ref = useRef();
+  const h2Ref = useRef();
+  const v1Ref = useRef();
+  const v2Ref = useRef();
+  const v3Ref = useRef();
+
+  useEffect(() => {
+    [h1Ref, h2Ref, v1Ref, v2Ref, v3Ref].forEach((ref) => {
+      if (ref.current) {
+        ref.current.play().catch((err) => {
+          alert("Autoplay blocked:", err);
+        });
+      }
+    });
+  }, []);
+
   return (
     <Grid container bgcolor={theme.colors.beige} p={1} spacing={1}>
       <Grid container spacing={1}>
         <Grid item size={isMobile ? 12 : 6}>
           <video
+            ref={h1Ref}
             style={{ width: "100%", height: "100%", objectFit: "cover" }}
             autoPlay
             muted
@@ -23,11 +42,11 @@ const VideoSection = () => {
             playsInline
           >
             <source src={horizontal1} type="video/mp4" />
-            Your browser does not support the video tag.
           </video>
         </Grid>
         <Grid item size={isMobile ? 12 : 6}>
           <video
+            ref={h2Ref}
             style={{ width: "100%", height: "100%", objectFit: "cover" }}
             autoPlay
             muted
@@ -36,7 +55,6 @@ const VideoSection = () => {
             playsInline
           >
             <source src={horizontal2} type="video/mp4" />
-            Your browser does not support the video tag.
           </video>
         </Grid>
       </Grid>
@@ -45,6 +63,7 @@ const VideoSection = () => {
         <Grid container size={12} justifyContent="space-evenly" height="60vh">
           <Grid item size={3} height="100%">
             <video
+              ref={v1Ref}
               style={{ width: "100%", height: "100%", objectFit: "cover" }}
               autoPlay
               muted
@@ -57,6 +76,7 @@ const VideoSection = () => {
           </Grid>
           <Grid item size={3} height="100%">
             <video
+              ref={v2Ref}
               style={{ width: "100%", height: "100%", objectFit: "cover" }}
               autoPlay
               muted
@@ -69,6 +89,7 @@ const VideoSection = () => {
           </Grid>
           <Grid item size={3} height="100%">
             <video
+              ref={v3Ref}
               style={{ width: "100%", height: "100%", objectFit: "cover" }}
               autoPlay
               muted
